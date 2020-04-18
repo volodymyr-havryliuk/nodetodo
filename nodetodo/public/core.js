1a4d964f665adb7c5131d59be032847c52c6a658
volodymyr.havryliuk <volodymyr.havryliuk@demicon.de>
2020-04-18 21:11:14 +0300

    Initial commit

---
+++ nodetodo/public/core.js
@@ -0,0 +1,54 @@
+var nodeTodo = angular.module("nodeTodo", []);
+
+function mainController($scope, $http) {
+  $scope.formData = {};
+
+  $scope.cos = "Ala ma kota";
+
+  // when landing on the page, get all todos and show them
+  $http
+    .get("/api/todos")
+    .success(function(data) {
+      $scope.todos = data;
+    })
+    .error(function(data) {
+      console.log("Error: " + data);
+    });
+
+  // when submitting the add form, send the text to the node API
+  $scope.createTodo = function() {
+    $http
+      .post("/api/todos", $scope.formData)
+      .success(function(data) {
+        $("input").val("");
+        $scope.todos = data;
+      })
+      .error(function(data) {
+        console.log("Error: " + data);
+      });
+  };
+
+  // update a todo after checking it
+  $scope.updateTodo = function(id) {
+    $http
+      .put("/api/todos/" + id)
+      .success(function(data) {
+        $scope.todos = data;
+      })
+      .error(function(data) {
+        console.log("Error: " + data);
+      });
+  };
+
+  // delete a todo after checking it
+  $scope.deleteTodo = function(id) {
+    $http
+      .delete("/api/todos/" + id)
+      .success(function(data) {
+        $scope.todos = data;
+      })
+      .error(function(data) {
+        console.log("Error: " + data);
+      });
+  };
+}
